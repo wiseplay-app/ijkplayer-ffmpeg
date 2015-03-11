FFmpeg/FFmpeg

avcodec/utils: Add case for jv to avcodec_align_dimensions2()
Fixes out of array accesses
Fixes: asan_heap-oob_12304aa_8_asan_heap-oob_4da4f3_300_intro.jv

Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

libavcodec/utils.c
@@ -387,6 +387,10 @@ void avcodec_align_dimensions2(AVCodecContext *s, int *width, int *height,
             w_align = 4;
             h_align = 4;
         }
+        if (s->codec_id == AV_CODEC_ID_JV) {
+            w_align = 8;
+            h_align = 8;
+        }
         break;
     case AV_PIX_FMT_BGR24:
         if ((s->codec_id == AV_CODEC_ID_MSZH) ||

wiseplay-app/ijkplayer-ffmpeg

(cherry picked from commit 105654e376a736d243aef4a1d121abebce912e6b)
Conflicts:
	libavcodec/utils.c

libavcodec/utils.c
@@ -182,6 +182,10 @@ void avcodec_align_dimensions2(AVCodecContext *s, int *width, int *height, int l
             w_align = 4;
             h_align = 4;
         }
+        if (s->codec_id == CODEC_ID_JV) {
+            w_align = 8;
+            h_align = 8;
+        }
         break;
     case PIX_FMT_BGR24:
         if((s->codec_id == CODEC_ID_MSZH) || (s->codec_id == CODEC_ID_ZLIB)){